932d0b80a679ca0514492b52fe8857dd4c435c42
Jason Kim <jasonkim@jasons-mbp-6.home>
2019-06-12 11:30:09 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='EDA and fight prediction classification for UFC fights.',
+    author='Jason Kim ',
+    license='MIT',
+)
